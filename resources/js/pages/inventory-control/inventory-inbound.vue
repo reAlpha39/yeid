37a0baf597ec073ce21c09a6e0056efa89d5974b
reAlpha39 <yunusw718@gmail.com>
2024-09-16 12:55:03 +0700

    improve: change endDate parameter to current date

--- resources/js/pages/inventory-control/inventory-inbound.vue
+++ resources/js/pages/inventory-control/inventory-inbound.vue
@@ -2,10 +2,21 @@
 // import data from '@/views/demos/forms/tables/data-table/datatable';
 
 // No need to repeat `https://localhost/api` now
+const now = new Date();
+
+const formatDate = (date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  console.log(`${year}${month}${day}`);
+  return `${year}${month}${day}`;
+};
+
 const data = await $api("/invControl", {
   params: {
     startDate: "20240417",
-    endDate: "20241231",
+    endDate: formatDate(now),
     jobCode: "I",
     limit: 0,
     orderBy: "jobdate",
