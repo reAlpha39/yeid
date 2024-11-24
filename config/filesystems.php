390e3079ede7d9fa89fb277a7cfa85ad67f301fe
reAlpha39 <yunusw718@gmail.com>
2024-11-24 15:11:14 +0700

    improve: add link public

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,9 +39,14 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
+            'links' => [
+                public_path('storage') => storage_path('app/public'),
+                // Add this line to create an additional link in public_html
+                '~/public_html/storage' => storage_path('app/public'),
+            ],
         ],
 
         's3' => [
