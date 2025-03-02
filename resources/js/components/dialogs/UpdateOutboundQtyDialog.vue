cc7cd09ccee35426410a4e915d5446600017ebfe
reAlpha39 <yunusw718@gmail.com>
2025-03-02 11:23:26 +0700

    improve: add error when selectedMachine is null on submit

--- resources/js/components/dialogs/UpdateOutboundQtyDialog.vue
+++ resources/js/components/dialogs/UpdateOutboundQtyDialog.vue
@@ -33,12 +33,18 @@ const dialogVisibleUpdate = (val) => {
   emit("update:isDialogVisible", val);
 };
 
+const form = ref();
 const machines = ref([]);
 const selectedMachine = ref(null);
 const quantity = ref("");
 
 async function saveUpdatedQty() {
   try {
+    const { valid, errors } = await form.value?.validate();
+    if (valid === false) {
+      return;
+    }
+
     await $api("/inventory/update-inv-outbound", {
       method: "POST",
       body: {
@@ -108,92 +114,95 @@ watch(
     <!-- 👉 Dialog close btn -->
     <DialogCloseBtn @click="$emit('update:isDialogVisible', false)" />
 
-    <VCard class="pa-2 pa-sm-10">
-      <VCardText>
-        <h4 class="text-h4 text-center mb-2">Adjustment Out-Bound Data</h4>
-      </VCardText>
+    <VForm ref="form" lazy-validation>
+      <VCard class="pa-2 pa-sm-10">
+        <VCardText>
+          <h4 class="text-h4 text-center mb-2">Adjustment Out-Bound Data</h4>
+        </VCardText>
 
-      <div class="pa-4 rounded" style="background-color: #4b3e6414">
-        <AppAutocomplete
-          v-model="selectedMachine"
-          placeholder="Machine"
-          item-title="title"
-          :items="machines"
-          return-object
-          outlined
-          style="background-color: #ffffff"
-        />
+        <div class="pa-4 rounded" style="background-color: #4b3e6414">
+          <AppAutocomplete
+            v-model="selectedMachine"
+            placeholder="Machine"
+            item-title="title"
+            :items="machines"
+            return-object
+            outlined
+            style="background-color: #ffffff"
+            :rules="[requiredValidator]"
+          />
 
-        <div class="pa-4 mt-4 rounded" style="background-color: #ffffff">
-          <VRow class="no-gutters">
-            <VCol cols="12" md="4"> Machine Name </VCol>
-            <VCol cols="12" md="8">
-              : {{ selectedMachine?.machinename ?? "-" }}
+          <div class="pa-4 mt-4 rounded" style="background-color: #ffffff">
+            <VRow class="no-gutters">
+              <VCol cols="12" md="4"> Machine Name </VCol>
+              <VCol cols="12" md="8">
+                : {{ selectedMachine?.machinename ?? "-" }}
+              </VCol>
+            </VRow>
+            <VRow class="no-gutters">
+              <VCol cols="12" md="4"> Shop Code </VCol>
+              <VCol> : {{ selectedMachine?.shopcode ?? "-" }} </VCol>
+            </VRow>
+            <VRow class="no-gutters">
+              <VCol cols="12" md="4"> Shop Name </VCol>
+              <VCol> : {{ selectedMachine?.shopname ?? "-" }} </VCol>
+            </VRow>
+            <VRow class="no-gutters">
+              <VCol cols="12" md="4"> Model Name </VCol>
+              <VCol> : {{ selectedMachine?.modelname ?? "-" }} </VCol>
+            </VRow>
+            <VRow class="no-gutters">
+              <VCol cols="12" md="4"> Maker Name </VCol>
+              <VCol> : {{ selectedMachine?.makername ?? "-" }} </VCol>
+            </VRow>
+            <VRow class="no-gutters">
+              <VCol cols="12" md="4"> Line Code </VCol>
+              <VCol> : {{ selectedMachine?.linecode ?? "-" }} </VCol>
+            </VRow>
+          </div>
+          <br />
+        </div>
+
+        <div
+          class="pa-4 my-4 rounded"
+          style="background-color: #e8776814; opacity: 8"
+        >
+          <VRow class="d-flex">
+            <VCol> Outbound Quantity </VCol>
+            <VCol>
+              <div style="text-align: right">
+                <strong>
+                  {{ formatNumber(props.quantity) }}
+                </strong>
+              </div>
             </VCol>
           </VRow>
-          <VRow class="no-gutters">
-            <VCol cols="12" md="4"> Shop Code </VCol>
-            <VCol> : {{ selectedMachine?.shopcode ?? "-" }} </VCol>
-          </VRow>
-          <VRow class="no-gutters">
-            <VCol cols="12" md="4"> Shop Name </VCol>
-            <VCol> : {{ selectedMachine?.shopname ?? "-" }} </VCol>
-          </VRow>
-          <VRow class="no-gutters">
-            <VCol cols="12" md="4"> Model Name </VCol>
-            <VCol> : {{ selectedMachine?.modelname ?? "-" }} </VCol>
-          </VRow>
-          <VRow class="no-gutters">
-            <VCol cols="12" md="4"> Maker Name </VCol>
-            <VCol> : {{ selectedMachine?.makername ?? "-" }} </VCol>
-          </VRow>
-          <VRow class="no-gutters">
-            <VCol cols="12" md="4"> Line Code </VCol>
-            <VCol> : {{ selectedMachine?.linecode ?? "-" }} </VCol>
-          </VRow>
         </div>
-        <br />
-      </div>
 
-      <div
-        class="pa-4 my-4 rounded"
-        style="background-color: #e8776814; opacity: 8"
-      >
         <VRow class="d-flex">
-          <VCol> Outbound Quantity </VCol>
-          <VCol>
-            <div style="text-align: right">
-              <strong>
-                {{ formatNumber(props.quantity) }}
-              </strong>
-            </div>
+          <VCol> Adjustment outbound to </VCol>
+          <VCol cols="12" md="2">
+            <AppTextField
+              v-model.number="quantity"
+              type="number"
+              placeholder="5"
+              min="0"
+            />
           </VCol>
         </VRow>
-      </div>
 
-      <VRow class="d-flex">
-        <VCol> Adjustment outbound to </VCol>
-        <VCol cols="12" md="2">
-          <AppTextField
-            v-model.number="quantity"
-            type="number"
-            placeholder="5"
-            min="0"
-          />
-        </VCol>
-      </VRow>
-
-      <VRow class="d-flex justify-start">
-        <VCol>
-          <VBtn class="me-4" @click.prevent="saveUpdatedQty()">Submit</VBtn>
-          <VBtn
-            variant="tonal"
-            color="error"
-            @click="$emit('update:isDialogVisible', false)"
-            >Cancel</VBtn
-          >
-        </VCol>
-      </VRow>
-    </VCard>
+        <VRow class="d-flex justify-start">
+          <VCol>
+            <VBtn class="me-4" @click.prevent="saveUpdatedQty()">Submit</VBtn>
+            <VBtn
+              variant="tonal"
+              color="error"
+              @click="$emit('update:isDialogVisible', false)"
+              >Cancel</VBtn
+            >
+          </VCol>
+        </VRow>
+      </VCard>
+    </VForm>
   </VDialog>
 </template>
